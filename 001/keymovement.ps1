cee80909cab165af050adb90bd904601edacf1b7
AlanSt <alanst@microsoft.com>
2015-12-01 19:11:44 -0800

    Update keymovement.ps1

--- 001/keymovement.ps1
+++ 001/keymovement.ps1
@@ -12,6 +12,6 @@ param(
 $DataKey > ".\$DataSA.sa"
  
 $context = New-AzureStorageContext -StorageAccountName $TransferSA -StorageAccountKey $TransferKey
-New-AzureStorageContainer -Name "sa" -Context $context
-Set-AzureStorageBlobContent -Container "sa" -File ".\$DataSA.sa" -Blob "$DataSA.sa" -Context $context -Force
+New-AzureStorageContainer -Name "sas" -Context $context
+Set-AzureStorageBlobContent -Container "sas" -File ".\$DataSA.sa" -Blob "$DataSA.sa" -Context $context -Force
 
