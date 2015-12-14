162472f15eb27f5f6512983adf6360b266555877
alanst <alanst@microsoft.com>
2015-12-14 03:36:31 -0800

    a

--- 001/upload.sh
+++ 001/upload.sh
@@ -1,7 +1,10 @@
 # accepts a storage account name and a key
 export AZURE_STORAGE_ACCOUNT="$1" 
+echo $1
 export AZURE_STORAGE_ACCESS_KEY="$2" 
+echo $2
 azure storage container create vhds
+echo "vhds created"
 
 blob_name=$(echo "$1" | cut -f5 -d/)
 blobxfer $1 vhds "/mnt/$blob_name" --storageaccountkey $2 --upload --no-computefilemd5 --autovhd
